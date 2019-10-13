815a4f8faf29d10eb875ba435315dc870e919113
kaoru.maruyama <marukao28@gmail.com>
2019-10-14 08:53:59 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 丸山薫 on 2019/10/14.
+//  Copyright © 2019 kaoru.maruyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
